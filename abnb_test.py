f91d6ef092a5e44745c60e45bbc4730dd937855f
Victoria Borsetti <135077759+vicotriangle@users.noreply.github.com>
2023-07-30 13:55:36 -0400

    Update abnb_test.py

--- abnb_test.py
+++ abnb_test.py
@@ -3,22 +3,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-
 st.title('AirBnB Price Predictor')
 st.markdown('Determine if you are charging the right amount for your AirBnB.')
 st.header('Enter the characteristics of your AirBnB:')
 
-
-room_type = st.selectbox("Room Type: ",
-                     ['Entire home/apt', 'Private room','Shared room','Hotel room'])
+room_type = st.selectbox("Room Type: ", ['Entire home/apt', 'Private room','Shared room','Hotel room'])
 host_is_superhost = st.radio("Are you a Superhost?: ", ('No', 'Yes'))
 accommodates = st.number_input("Enter max of occupancy: ", step=1)
 bedrooms = st.number_input("Enter number of bedrooms: ", step=1)
 new_bathroom = st.number_input("Enter number of bathrooms: ", step=1)
 review_scores_rating = st.number_input("Enter current AirBnB rating: ")
-amenities = st.multiselect("Amenities: ",
-                         ['TV', 'Kitchen', 'Washer'])
-Neighbourhood_group_cleansed = bedrooms = st.selectbox("Select Borough: ",
-                     ['Manhattan','Queens', 'Brooklyn' ,'Bronx'])
+amenities = st.multiselect("Amenities: ", ['TV', 'Kitchen', 'Washer'])
+Neighbourhood_group_cleansed = bedrooms = st.selectbox("Select Borough: ", ['Manhattan','Queens', 'Brooklyn' ,'Bronx'])
+
 st.button("Predict Price")
